b1e1f1b055b1028ea5737c6f248de369ef4e26de
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-20 17:28:02 +0600

    user api created for DB

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const {MongoClient, ServerApiVersion} = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
-/* mdtanim0195
-SBnHpsEMCFudXDjx
- */
-const port = process.env.PORT || 3000;
 
-const uri = "mongodb+srv://mdtanim0195:dhur123@cluster0.ogpr4db.mongodb.net/?retryWrites=true&w=majority";
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${
+    process.env.DB_USER
+}:${
+    process.env.DB_PASS
+}@cluster0.ogpr4db.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,35 +28,49 @@ const client = new MongoClient(uri, {
 async function run() {
     try { // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        //collection
+        const productCollection = client.db("productDB").collection("product");
+        const userCollection = client.db("userDB").collection("user");
 
-        const userCollection = client.db("userDB").collection("users");  
-        // post user
-        app.post("/users", async(req, res) =>{
-            const user = req.body;
-            const result = await userCollection.insertOne(user);
-            console.log(result);
+        // post product
+        app.post("/product", async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
             res.send(result);
+
         });
-        //get user
-        app.get("/users", async (req, res) =>{
-            const result = await userCollection.find().toArray();
-            console.log(result);
+        // get product
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
             res.send(result);
         });
         // delete user
-        app.delete("/users/:id", async (req, res) =>{
+        app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
             console.log(id);
         });
+        // user api
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ping: 1});
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally { // Ensures that the client will close when you finish/error
-        await client.close();
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
+
+// rooot
 app.get("/", (req, res) => {
     res.send("Card is running...");
 });
